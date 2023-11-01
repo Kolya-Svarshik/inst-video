5573d82943dc834420e49e1ecdb3b65c60afdb83
Kolya Svarshik <kolya-svarshik@github.ccom>
2023-11-01 21:52:09 +0200

    edit logic algorithm

--- src/js/index.js
+++ src/js/index.js
@@ -4,33 +4,37 @@ const btnAdd = document.querySelector("button[add-link]");
 const btnReload = document.querySelector("button[reload-link]");
 const inputNewLink = document.querySelector("input[input-new-link]");
 
+btnClean.addEventListener("click", () => {
+  // clean input
+  oldInputLink.value = "";
+  inputNewLink.value = "";
+});
+
 btnAdd.addEventListener("click", () => {
   // push link
   navigator.clipboard.readText().then(function (clipboardText) {
     oldInputLink.value = clipboardText;
   });
 
-  btnClean.addEventListener("click", () => {
-    // clean input
-    oldInputLink.value = "";
-    inputNewLink.value = "";
-  });
-
   setTimeout(() => {
-    // transform
-    let link = oldInputLink.value.split("");
-    const delLink = link.splice(8, 4, "d", "d");
-    link = link.join("");
-
-    // record new link
-    inputNewLink.value = link;
-
-    // copy new link
-    navigator.clipboard.writeText(link);
+    transformLink();
   }, 10);
 });
 
 btnReload.addEventListener("click", () => {
+  transformLink();
+});
+
+function transformLink() {
+  // link verification
+  let verificationLink = oldInputLink.value.split("");
+  const delverificationLink = verificationLink.splice(25);
+  verificationLink = verificationLink.join("");
+
+  if (verificationLink !== "https://www.instagram.com") {
+    return navigator.clipboard.writeText("Це не посилання з Instagram!");
+  }
+
   // transform
   let link = oldInputLink.value.split("");
   const delLink = link.splice(8, 4, "d", "d");
@@ -41,4 +45,4 @@ btnReload.addEventListener("click", () => {
 
   // copy new link
   navigator.clipboard.writeText(link);
-});
+}
